cfda6fbb9b8916873cbbbca94deb5d99bb86228e
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-10-01 00:25:00 +0530

    Update SecurityConfig.java

--- src/main/java/in/expedia/security/SecurityConfig.java
+++ src/main/java/in/expedia/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package in.expedia.security;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 import static org.springframework.security.config.Customizer.withDefaults;
